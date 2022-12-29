bfb2883c9053018c7f5d63606996fd155ebf698b
leozeyad <87402468+leozeyad@users.noreply.github.com>
2022-12-29 23:25:57 +0200

    useless 6

--- app/Models/Mycart.php
+++ app/Models/Mycart.php
@@ -20,7 +20,7 @@ class Mycart extends Model
     ];
     public $timestamps = false;
 
-    public function product(){
-        return $this->belongsTo(Product::class);
+    public function car(){
+        return $this->belongsTo(car::class);
     }
 }
